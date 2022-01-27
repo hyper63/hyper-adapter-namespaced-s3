2ceaf0aeed01b34f27f7e5fb1b8681d4ec5a5a9f
Tyler Hall <tyler@hyper.io>
2022-01-27 14:33:32 -0500

    chore: pull in fork to fix error with deleteObjects

--- deps.js
+++ deps.js
@@ -5,7 +5,8 @@ export {
   ApiFactory,
   AwsEndpointResolver,
 } from "https://deno.land/x/aws_api@v0.6.0/client/mod.ts";
-export { S3 } from "https://deno.land/x/aws_api@v0.6.0/services/s3/mod.ts";
+// Use my fork until https://github.com/cloudydeno/deno-aws_api/pull/27 is merged
+export { S3 } from "https://raw.githubusercontent.com/TillaTheHun0/deno-aws_api/fix/s3-delete-objects-missing-header/lib/services/s3/mod.ts";
 export { readAll } from "https://deno.land/std@0.122.0/streams/conversion.ts";
 export { Buffer } from "https://deno.land/std@0.122.0/io/buffer.ts";
 export { crypto } from "https://deno.land/std@0.122.0/crypto/mod.ts";
