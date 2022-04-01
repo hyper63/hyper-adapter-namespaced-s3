9f252b3d383fcc05530956547e08978a0ff1d0b8
Tyler Hall <tyler@hyper.io>
2022-04-01 12:24:27 -0400

    chore: bump deps

--- deps.js
+++ deps.js
@@ -8,10 +8,9 @@ export {
   DefaultCredentialsProvider,
 } from "https://deno.land/x/aws_api@v0.6.0/client/mod.ts";
 export { S3 } from "https://raw.githubusercontent.com/cloudydeno/deno-aws_api/efe1a0bc6a9c6ece3ba02133e10a79446fb92f58/lib/services/s3/mod.ts";
-export { readAll } from "https://deno.land/std@0.128.0/streams/conversion.ts";
-export { Buffer } from "https://deno.land/std@0.128.0/io/buffer.ts";
-export { crypto } from "https://deno.land/std@0.128.0/crypto/mod.ts";
-export { join } from "https://deno.land/std@0.128.0/path/mod.ts";
+export { readAll } from "https://deno.land/std@0.132.0/streams/conversion.ts";
+export { Buffer } from "https://deno.land/std@0.132.0/io/buffer.ts";
+export { join } from "https://deno.land/std@0.132.0/path/mod.ts";
 
 export {
   HyperErr,
--- dev_deps.js
+++ dev_deps.js
@@ -1,5 +1,5 @@
 // Schema parsing deps
-export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.0.1/utils/plugin-schema.js";
+export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.1.0/utils/plugin-schema.js";
 export { storage as validateStorageAdapterSchema } from "https://x.nest.land/hyper-port-storage@1.2.0/mod.js";
 
 // std lib deps
@@ -8,6 +8,6 @@ export {
   assertEquals,
   assertObjectMatch,
   assertThrows,
-} from "https://deno.land/std@0.128.0/testing/asserts.ts";
+} from "https://deno.land/std@0.132.0/testing/asserts.ts";
 
-export { spy } from "https://deno.land/x/mock@0.13.0/mod.ts";
+export { spy } from "https://deno.land/x/mock@0.15.0/mod.ts";
