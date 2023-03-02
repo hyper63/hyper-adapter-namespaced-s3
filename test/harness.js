67892c9fa0044fe445cde735d070e4d61532cef4
Tyler Hall <tyler@hyper.io>
2023-03-02 23:45:51 +0000

    chore: bump deps and refactor usages #24

--- test/harness.js
+++ test/harness.js
@@ -1,8 +1,9 @@
 // Load .env
-import 'https://deno.land/x/dotenv@v2.0.0/load.ts'
-import { default as appOpine } from 'https://x.nest.land/hyper-app-opine@2.2.0/mod.js'
-import { default as core } from 'https://x.nest.land/hyper@3.3.0/mod.js'
+import 'https://deno.land/std@0.178.0/dotenv/load.ts'
 import { cuid } from 'https://deno.land/x/cuid@v1.0.0/index.js'
+
+import { default as appOpine } from 'https://x.nest.land/hyper-app-opine@2.3.0/mod.js'
+import { default as core } from 'https://x.nest.land/hyper@3.4.2/mod.js'
 
 import myAdapter from '../mod.js'
 import PORT_NAME from '../port_name.js'
