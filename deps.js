ecc57284f51c1282dd1d436096fdd7211dffbfc4
Tyler Hall <tyler@hyper.io>
2023-05-23 21:44:47 -0400

    refactor: remove nest.land imports. cleanup workflows

--- deps.js
+++ deps.js
@@ -9,6 +9,9 @@ export {
 export { S3 } from 'https://deno.land/x/aws_api@v0.8.1/services/s3/mod.ts'
 export { managedUpload } from 'https://deno.land/x/aws_api@v0.8.1/extras/s3-upload.ts'
 
-export { join } from 'https://deno.land/std@0.182.0/path/mod.ts'
+export { join } from 'https://deno.land/std@0.187.0/path/mod.ts'
 
-export { HyperErr, isHyperErr } from 'https://x.nest.land/hyper-utils@0.1.0/hyper-err.js'
+export {
+  HyperErr,
+  isHyperErr,
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
